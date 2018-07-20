0c493a31c83bc1e42a5713cabba326561dcdce9b
Jim M <Jim.R.Maloney@gmail.com>
2018-07-19 23:43:03 -0400

    Updating files

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -34,7 +34,7 @@ function showAllProducts() {
       connection.query(query, function(err, res) {
         for (var i = 0; i < res.length; i++) {
           // for (var i = 0; i < 8; i++) {
-          console.log("item_id " + res[i].item_id + " || Product Name " + res[i].product_name + " || Department " + res[i].department_name);
+          console.log("item_id " + res[i].item_id + " || Product Name " + res[i].product_name + " || Department " + res[i].department_name + "|| Price " + res[i].price );
         }
         bidAuction();
       });
@@ -44,7 +44,6 @@ function showAllProducts() {
 function bidAuction() {
   // query the database for all items in products
   connection.query("SELECT * FROM products", function(err, results) {
-    console.log("Pick an item number")
     if (err) throw err;
     // once you have the items, prompt the user for which they'd like to bid on
     inquirer
@@ -55,10 +54,9 @@ function bidAuction() {
           choices: function() {
             var choiceArray = [];
             for (var i = 0; i < results.length; i++) {
-              choiceArray.push(results[i].item_id);
+              choiceArray.push(results[i].product_name);
               
             }
-            console.log(choiceArray);
             return choiceArray;
             
           },
@@ -107,4 +105,5 @@ function bidAuction() {
       });
   });
 }
+
 
